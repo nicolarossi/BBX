4a5d21b95466d7667c903ab1549535a454fad3d5
Nicola <nicola.rossi+2@gmail.com>
2017-11-17 19:31:09 +0100

    Centered img in carousel

--- WebContent/script_carousel.sh
+++ WebContent/script_carousel.sh
@@ -1,4 +1,20 @@
 
-find images/carousel -type f |sort |while read f ; do
-echo '<img class="mySlides w3-animate-fading img img-rounded" src="'$f'" style="width:100%">'
+#find images/carousel -type f |sort |while read f ; do
+#echo '<img class="mySlides w3-animate-fading img img-rounded" src="'$f'" style="width:100%">'
+#done
+
+active="active"
+for ((i=1;i<=30;i++)); do
+    IMG=$(printf "img/slide_%02d.png" $i)
+
+    echo "
+    <div class=\"item $active\">
+     <div class='row'>
+       <p class='text-center'>
+	<img src=\"${IMG}\" class='img-rounded' alt='Logo' >
+       </p>
+       </div>
+       </div>"
+
+    active=""    
 done
