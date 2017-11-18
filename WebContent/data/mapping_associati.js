c55bd2666530e86ecf0030271e49c96e1d96b331
Debian <debian@maya-logbook.com>
2017-11-18 14:35:57 +0000

    Surname

--- WebContent/data/mapping_associati.js
+++ WebContent/data/mapping_associati.js
@@ -1,30 +1,30 @@
 var associati=[
-    { name : "Giuseppe" ,
+    { name : "Giuseppe R." ,
       level : "Tech 2",
       image : "images/associati/rodio.jpg",
       female : "false"
     }
-    , { name : "Silvia" ,
+    , { name : "Silvia M." ,
 	level : "Fundamental",
 	image : "images/associati/Silvia_Mastrocesare.jpg",
 	female : "false"
       }
-    , { name : "Maurizio" ,
+    , { name : "Maurizio C." ,
 	level : "Fundamental",
 	image : "images/associati/Maurizio.jpg",
 	female : "false"
       }
-    , { name : "Eugenia" ,
+    , { name : "Eugenia P." ,
 	level : "Fundamental",
 	image : "images/associati/eugenia.jpg",
 	female : "false"
       }
-    , { name : "Gonzalo" ,
+    , { name : "Marco A.G." ,
 	level : "Fundamental",
 	image : "images/associati/gonzalo.jpg",
 	female : "false"
       }
-    , { name : "Nicola" ,
+    , { name : "Nicola R." ,
 	level : "Rec 3",
 	image : "images/associati/nicola.jpg",
 	female : "false"
