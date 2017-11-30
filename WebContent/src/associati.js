41195abf4f035d86660e6b9f8e8747dfd204a979
Nicola Rossi <nicola.rossi+2@gmail.com>
2017-11-30 18:34:03 +0000

    Fix

--- WebContent/src/associati.js
+++ WebContent/src/associati.js
@@ -8,11 +8,16 @@ function load_associati() {
 	var panel = $("<div class='panel panel-transparent'></div>");
 
 	var header=$("<div class='panel-heading'></div>");
+//	header.html("<div class='row'><div class='col-xs-6'><strong><h2>"+socio.name+"</h2></div><div class='col-xs-6'></strong><i><h3>"+socio.level+"</h3></i></div>");
+	//	header.html("<strong><h2>"+socio.name+"</h2></strong><i><h3>"+socio.level+"</h3></i>");
 	header.html("<strong><h2>"+socio.name+"</h2></strong>");
 
 	var body=$("<div class='panel-body'></div>");
-	body.html("<img class='img img-responsive img-rounded' src='"+socio.image+"'>");
+	body.html("<img class='img img-responsive img-rounded' src='"+socio.image+"'><br><ul><li><strong>"+socio.level+"</strong></li></ul>");
 
+//	var footer=$("<div class='panel-footer'></div>");
+//	footer.html("<ul><li><strong>"+socio.level+"</strong></li></ul>");
+		   
 	panel.append(header,body);
 
 	el.append(panel);
