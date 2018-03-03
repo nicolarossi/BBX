bfcaebd3bf60a59cf88b5e637497cdc77bdd2d84
Nicola <nicola.rossi+2@gmail.com>
2018-03-03 16:36:02 +0100

    Added tool for dropbox syncing

---
+++ WebContent/tool/dropbox_exclude.sh
@@ -0,0 +1,21 @@
+DROPBOX_PY=~/Project/BBX/WebContent/tool/dropbox.py
+
+python ${DROPBOX_PY} exclude add Miky
+python ${DROPBOX_PY} exclude add Musica
+python ${DROPBOX_PY} exclude add PP
+python ${DROPBOX_PY} exclude add Prova
+python ${DROPBOX_PY} exclude add "Villa d'Este Notturna"
+python ${DROPBOX_PY} exclude add "Villa d' Este"
+python ${DROPBOX_PY} exclude add "website project"
+python ${DROPBOX_PY} exclude add PR
+python ${DROPBOX_PY} exclude add Apps
+python ${DROPBOX_PY} exclude add Certificationi_PPIT
+python ${DROPBOX_PY} exclude add Cucina
+python ${DROPBOX_PY} exclude add CV
+python ${DROPBOX_PY} exclude add Certificazioni_PPIT
+python ${DROPBOX_PY} exclude add Public
+python ${DROPBOX_PY} exclude add resume
+python ${DROPBOX_PY} exclude add Scansioni
+python ${DROPBOX_PY} exclude add Silvia
+python ${DROPBOX_PY} exclude add tesi
+
