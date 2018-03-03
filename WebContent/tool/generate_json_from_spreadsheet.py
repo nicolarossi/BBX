6a74deba052e2ebea0a245196b9285d5d854c3c5
Nicola <nicola.rossi+2@gmail.com>
2018-03-03 16:35:14 +0100

    Imported from dropbox

--- WebContent/tool/generate_json_from_spreadsheet.py
+++ WebContent/tool/generate_json_from_spreadsheet.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import urllib,sys, json
+import urllib,sys, json,os
 from openpyxl import load_workbook
+from authentication_data import config_authentication
+from wand.image import Image
 
 def print_json(data,path):    
     with open(path, 'w') as outfile:
         json.dump(data, outfile)
     
 def import_site(sheet,features,maps):
-    print 'Import stations name:'
+    print('Import stations name:')
 
     for i in range(2,100):
         cell_key='B'+str(i)
@@ -71,10 +73,19 @@ def import_site(sheet,features,maps):
         
 
 
-def import_measurement(sheet,start_column_measurement,maps,measurement):
-    print 'Import measurement :'
+def import_measurement(sheet,start_column_measurement,maps,measurement,dropbox_folder,image_folder):
+    name_of={}
+
+    for dive_site in maps:
+        for dive_station in maps[dive_site]:
+            name_of[dive_station]=dive_site
+
+    dropbox_radix="ProjectBaselineTyrrhenianSea/photos"
+
+    print('Import measurement :')
 
     for i in range(2,100):
+        print('Measurement/Row : '+str(i))
         id_stations=sheet['B'+str(i)].value
         if (id_stations is None):
             print('WARNING: Cell B'+str(i)+' is empty it was expected?')
@@ -92,33 +103,110 @@ def import_measurement(sheet,start_column_measurement,maps,measurement):
         column='C'
         j=0
         for dim in list_measures:
-#            print(x+' on column '+column )
             val_dim=sheet[column+str(i)].value
             if (val_dim is None):
                 continue
 
             if (dim == "Time"):
-                measure[dim]=''+str(val_dim.hour)+':'+str(val_dim.minute)
+                try :
+                    measure[dim]=''+str(val_dim.hour)+':'+str(val_dim.minute)
+                except AttributeError:
+                    f=val_dim.split(':')                    
+                    try :
+                        measure[dim]=''+f[0]+':'+f[1]
+                    except AttributeError:
+                        print(' ERROR on row '+str(i)+' for column Time ')
+                        raise
+                          
             elif (dim == "Date") :
                 measure[dim]=''+str(val_dim.year)+'/'+str(val_dim.month)+'/'+str(val_dim.day)
+            elif (dim == "URL/Photo Name"):
+                # TODO
+                # * costruire il path
+                # * costruire l'alberatura locale
+                # * scaricare il file
+                # se non riesce a scaricare il file lancia un errore
+                # * convertirlo in dimensione voluta
+                # * mettere nel json la url
+                measure['url']=''+name_of[id_stations]+'/'+val_dim+''
+
+                if ((not (measure['url']).endswith('.jpg')) and (not (measure['url']).endswith('.jpeg'))):
+                    measure['url']+='.jpg'
+                
             else:
                 measure[dim]=val_dim
 
             column=chr(ord(column)+1)
 
-            
-        #--- insert in the local set of measurement 
+
+        #---- Creare l'alberatura
+        import_image_from_dropbox(dropbox_folder,image_folder,measure)
+
+        #--- insert in the local set of measurement
+        
         v.append(measure)
 
         #--- update the measurement for the stations read
         measurement[id_stations]=v
-        
+
+def import_image_from_dropbox(dropbox_folder,image_folder,measure):
+    
+    print(' Import image from dropbox folder...')
+    dropbox_path=dropbox_folder+measure['url']
+
+    dest_format='png'
+    
+    tmp=image_folder+measure['url']
+
+    dest_dir=os.path.dirname(tmp)
+            
+    tmp_dest_file = os.path.basename(image_folder+measure['url'])
+    
+    (dest_file,tmp_ext)=os.path.splitext(tmp_dest_file)
+    
+    dest_path=dest_dir+'/'+dest_file+'.'+dest_format
+
+    dropbox_path=dropbox_path.replace('//','/')
+    dest_path=dest_path.replace('//','/')
+
+    print(' from : ['+dropbox_path+']')
+    print(' to   : ['+dest_path+']')
+    
+    if (not os.path.exists(dest_dir)):
+        os.makedirs(dest_dir)
+
+    # Convert in dest_format
+    if (not os.path.exists(dropbox_path)):
+        print('ERRORE ')
+        print('il file ['+dropbox_path+'] non e'' presente')
+        sys.exit(-1)
+                
+    with Image(filename=dropbox_path) as original:
+        with original.clone() as converted:
+            converted.format=dest_format
+            converted.transform(resize='400x')
+            
+            converted.save(filename=dest_path)
+            #                    (w,h)=original.size()
+            #                    d_h= d_w*h/w
+            #                   converted.resize(d_w,int(d_h))
+            print('')
+
+
+
 
 def main(argv):
     #------
     #--- Config parameter
     #-
-    URL="https://docs.google.com/spreadsheets/d/1tSenu0DwUBuS66SxtqrxQfD25DnUPj4PI_2IafLin0M/export?format=xlsx"
+    auth=config_authentication()
+    URL=auth['URL']
+    app_key=auth['app_key']
+    app_secret=auth['app_secret']
+    dropbox_token=auth['dropbox_token']
+    dropbox_folder=auth['src_dropbox']+'/'
+    image_folder=auth['dst_image']+'/'
+
     path_excel='pippo.xlsx'
 
     stations_title='Stazioni'
@@ -140,16 +228,18 @@ def main(argv):
     #print wb.get_sheet_names()
     maps={}
     
+
     for sheet in wb:
         if stations_title == sheet.title:
             import_site(sheet,features,maps)
             continue
 
 
+
     measurement={}
     for sheet in wb:
         if measurement_title == sheet.title:
-            import_measurement(sheet,start_column_measurement,maps,measurement)
+            import_measurement(sheet,start_column_measurement,maps,measurement,dropbox_folder,image_folder)
 
     #
     dive_site['features'] = features
@@ -159,9 +249,18 @@ def main(argv):
     print_json(maps,'data/maps.json')
     print_json(measurement,'data/measurement.json')
 
+    #-----
+    #--- Download jpeg from dropbox and resize it ?
+    #-
+    
+#    print('user :'+str(dbx.users_get_current_account()))
+
+
+    
    
 if __name__ == "__main__":
     main(sys.argv)
         
 
     
+1
