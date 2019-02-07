e7fadb8ace510a6d7e52114d0c175cd34e0c0bbf
Nicola <nicola.rossi+2@gmail.com>
2019-02-07 21:45:32 +0100

    Change

--- WebContent/data/news.js
+++ WebContent/data/news.js
@@ -13,7 +13,7 @@ news_list=[
 	type : 'leaf',
 	date : '2018/09/02',
 	title : "Articolo su &quot;Il Tirreno&quot;",
-	body : 'In data 2018/09/02, a pg. VIII dell&apos; edizione cartacea de &quot;Il Tirreno&quot; &egrave; stato redatto un articolo a 6 colonne, per descrive il nostro intervento di mappatura e raccolta dati de Project Baseline Argentarola.',
+	body : 'In data 2018/09/02, a pg. VIII dell&apos; edizione cartacea de &quot;Il Tirreno&quot; &egrave; stato redatto un articolo a 6 colonne, per descrivere il nostro intervento di mappatura e raccolta dati de Project Baseline Argentarola.',
 	img: "img/news/tirreno.png",
 	link: "data/tirreno.pdf"
     },
@@ -25,6 +25,17 @@ news_list=[
 	body : "Due telecamere, quattro mesi di osservazioni, una fotografia ogni 15 minuti, oltre 16.000 immagini da analizzare: &#xE8; il risultato del progetto Dendrophyllia ramea portato avanti da <a href=\"www.blueresearch.eu\">Blueresearch</a> in collaborazione con i subacquei della <a href=\"www.gue.com\">Global Underwater Explorers</a> e <a href=\"www.bigbluexplorers.com\">BigBlueXplorers</a>.<br><em>Dendrophyllia ramea</em> &#xE8; una grande madrepora del mar Mediterraneo, molto poco conosciuta, cresce negli ambienti medio profondi, formando magnifiche colonie a forma di cespuglio alto anche 80 centimetri; lo scheletro &#xE8; spesso e robusto, con un acceso colore arancione, i polipi sono molto grandi di colore chiaro. Il progetto si &#xE8; concentrato sulla documentazione della biologia di questa specie mediante l&#x2019;utilizzo di una telecamera in grado di riprendere (mediante foto e videocamera) sia il corallo che l&#x2019;ambiente circostante per un lungo periodo di tempo, raccogliendo nel frattempo informazioni sulla temperatura e salinit&#xE0; dell&#x2019;ambiente in cui vive.<br>La strumentazione, costruita ad hoc da BlueResearch, &#xE8; stata posizionata in due diversi siti da subacquei BBX nel tratto di mare antistante Siracusa con il supporto logistico di <a href=\"http://www.capomurrodivingcenter.it/\">Capo Murro Diving Center</a> . Adesso, dopo oltre tre mesi in acqua, le telecamere sono state finalmente recuperate e le immagini disponibili serviranno a far luce sui ritmi di apertura e chiusura dei polipi come anche a capire se esista o meno una correlazione con orario, masse d&#x2019;acqua e correnti sottomarine diverse.",
 	img: "media/dendrophyllia_project_team.jpg",
 	link: "http://www.ansa.it/sito/videogallery/italia/2018/12/20/ecco-la-reine-dei-coralli_c4c399f8-d8e9-4082-92dc-cc2ca1f37742.html"
+    }
+,
+    {
+	id : 4 ,
+	type : 'leaf',
+	date : '2019/02/05',
+	title : "Continua il progetto sulla <em>Dendrophylia ramea</em>",
+	body : "In data 5 Febbraio 2019, sul sito di RaiNews 24, il canale informativo della tv nazionale, ha publicato un articolo per descrivere il progetto <em>Dendrophylia ramea</em><br>.
+ In Italia questa specie è rimasta in penombra per moltissimi anni - solo qualche esemplare raccolto dai pescatori e custodito nei musei - fino a quando non sono iniziate le prime ricerche con i robot filoguidati (ROV) e le esplorazioni dei primi subacquei tecnici capaci di immergersi fino a 80 metri di profondit&agrave;",
+	img: "media/500x500_1549366182112.DENDROPHYLLIA_1.png",
+	link: "http://www.rainews.it/dl/rainews/media/Quattro-mesi-sotto-acqua-per-uno-sguardo-unico-sugli-abissi-ecco-la-regina-dei-coralli-213b513b-5673-49e9-806d-9b829b6e7879.html#foto-1"
     }
 
 
