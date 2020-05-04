1cddd2360a44b1ec7f66567474151c9b971cd6eb
Nicola <nicola.rossi+2@gmail.com>
2020-05-04 20:48:54 +0200

    Change

--- WebContent/data/news.js
+++ WebContent/data/news.js
@@ -45,8 +45,7 @@ news_list=[
 	body : 'Un video sul progetto Dendrophyllia ramea svolto con il supporto scientifico di Eva Salvati, che abbiamo presentato alla 5a edizione della Conferenza Europea sull&apos; immersione scientifica. Subacquei BBX e GUE hanno partecipato al progetto. Claudio Provenzani, Andrea D&apos;Ambrosi, Fabio Portella, Stefano Gualtieri, Massimiliano Sabatini, Elena Romano. Blue Research staff: William Santero, Francesco Leonini, Simona Sorrenti. Un ringraziamento speciale a Capo Murro Diving Center per il supporto logistico.',
 	img: "img/news/euconf2019_eva.png",
 	link: "https://vimeo.com/331839972/2c6d62b70d"
-    }
-,
+    },
     {
 	id : 6 ,
 	type : 'education',
@@ -55,17 +54,57 @@ news_list=[
 	body : "Mercoled&#xEC; 25 Marzo dalle 19:30 alle 21:30 si &egrave; svolta la prima serata virtuale organizzata dalla <a href=\"http://www.bigbluexplorers.com\">BigBlueXplorers</a> dedicata all'archeologia subacquea con la presentazione, tenuta da Francesco &quot;Chicco&quot; Spaggiari dei progetti GUE delle Isole Eolie ed Egadi. I soci della BBX durante la presentazione hanno avuto la possibilita' di intervenire con delle domande su un tema cos&igrave; interessante per tutti i subacquei. Queste isole della Sicilia custodiscono un patrimonio archeologico unico al mondo e importanti reperti, che risalgono addirittura al 241 a.c., vengono scoperti ogni anno grazie alle attivita' portate avanti dai subacquei della <a href=\"http://www.gue.com\">Global Underwater Explorers</a> sotto il controllo della <a href=\"http://www.regione.sicilia.it/beniculturali/archeologiasottomarina\">Soprintendenza del Mare Regione Siciliana</a>.<br><br>Questi appuntamenti virtuali, riservati ai soci della BBX, proseguiranno con cadenza settimanale con altre interessanti presentazioni durante le quali verranno trattati, tra gli altri argomenti, la decompressione, la manutenzione delle attrezzature subacquee, l'esplorazione delle grotte subacquee in Sardegna ed alcuni soci della BBX racconteranno le pi&uacute; recenti partecipazioni ad alcuni corsi della Global Underwater Explorers (Sidemount, CCR1 e RB80).<br><br>Il prossimo appuntamento con la BBX_Virtual_Dive#2 &egrave; fissato per mercoled&igrave; 1 aprile 2020 alle ore 19:30, durer&agrave; fino alle 21:00, avr&agrave; come argomento la &#x22;Decompressione&#x22; presentato da #MarioArena.",
 	img: "img/news/virtual_dive_1.png",
 	link: "https://www.archeoxmed.org/"
-    }
-,
+    },
     {
 	id : 7 ,
 	type : 'education',
-	date : '2020/04/05',
+	date : '2020/04/01',
 	title : "BBX Virtual Dive #2",
 	// <a href=\"\"></a>
 	body : "Mercoled&#xEC; 1 Aprile dalle 19:30 alle 21:00 si e&#x2019; svolta la seconda serata virtuale organizzata dalla <a href=\"www.bigbluexplorers.com\">BigBlueXplorers</a>  dedicata stavolta alla decompressione, con una presentazione magistralmente tenuta da Mario Arena.<br><a href=\"www.gue.com/diver-training/gue-instructors/instructor-resume?id=515\">Mario Arena</a> e&#x2019; prima di tutto un esploratore, un esperto professionista dell&#x27;attivit&#xE0;&#x2019; subacquea nonch&#xE9; un carissimo amico per molti dei soci della BBX.<br>Appassionato esploratore subacqueo e Instructor Evaluator (IE) della <a href=\"www.gue.com\">Global Underwater Explorers</a> , Mario &#xE8; colui che ha reso possibile portare l&#x2019;addestramento GUE in Italia contribuendo attivamente a divulgare il nostro sistema di immersione.<br>La sua passione per la storia navale e la sua attivit&#xE0; di esplorazione lo hanno portato a scoprire ed identificare molti relitti di importanza storica, molti dei quali situati in Croazia e tra la Sicilia, Tunisia e Libia.<br>Mario organizza e coordina anche diversi progetti GUE in collaborazione con la Soprintendenza del <a href=\"http://www.regione.sicilia.it/beniculturali/archeologiasottomarina\">Mare Regione Siciliana</a> tra cui il progetto <a href=\"www.archeoxmed.org\">ARCHEOXMED</a>  che &#xE8; stato presentato nel precedente incontro da Francesco Spaggiari. <br>Durante la presentazione dal titolo &#x201C;Deco&#x26;Soda&#x2026; In Quarantena times&#x201D;, Mario Arena ha aggiornato i partecipanti in relazione all&#x2019;uso dei deep stop, dalla loro introduzione fino ad arrivare ai pi&#xF9; recenti studi e sperimentazioni. La presentazione &#xE8; stata arricchita dal confronto tra i diversi profili decompressivi in relazione ai vari algoritmi e livelli di gradienti/conservatismo, coprendo praticamente tutti i tipi di immersione, dalle pi&#xF9; semplici di livello Fondamentali fino ad arrivare a quelle pi&#xF9; complesse e aggressive di livello Tech 2 plus/RB80/CCR, passando per quelle Tech 1 e Tech 2.<br>Mario ha fatto riferimento anche ad una serie di recenti articoli del fondatore e presidente della <a href=\"www.gue.com/diver-training/gue-instructors/instructor-resume?id=13\">GUE Jarrod Jablonski</a>, pubblicati recentemente da <a href=\"https://gue.com/blog/author/admin\">GUE&#x2019;s InDepth</a>, dedicati appunto alla decompressione, alle soste profonde ed alla ricerca di tecniche diverse in un mondo molto complesso.<br>I soci della BBX, particolarmente numerosi per questa occasione, durante e al termine della presentazione hanno avuto la possibilit&#xE0; di intervenire con diverse domande su un tema decisamente interessante ed attuale per tutti i subacquei, specie nel momento in cui si passa dalle immersioni pi&#xF9; semplici a quelle pi&#xF9; profonde, pi&#xF9; lunghe e quindi pi&#xF9; complesse. <br>Gli appuntamenti virtuali, riservati ai soci della BBX ed agli Istruttori della GUE, proseguono con cadenza settimanale; il prossimo appuntamento con la BBX_Virtual_Dive#3 &#xE8; fissato per mercoled&#xEC; 8 aprile 2020 dalle ore 19.30 alle 21.00 durante il quale ci sposteremo stavolta sulla &#x22;subacquea culturale!&#x201D;, con la presentazione del nostro socio Mattia Musella incuriosendoci molto in quanto ci parler&#xE0; di:<br>Come &#xE8; nata la nostra bellissima passione? <br>Storia della subacquea: fatti, miti, leggende e curiosit&#xE0; del passato.",
 	img: "img/news/deco_soda_Q_times.png",
 	link: "https://www.gue.com/diver-training/gue-instructors/instructor-resume?id=515"
+    },
+    {
+	id : 8 ,
+	type : 'education',
+	date : '2020/04/08',
+	title : "BBX Virtual Dive #3",
+	body : 'Continuano i nostri incontri virtuali, riservati ai soci della BBX.<br> Mercoledì 8 aprile 2020 dalle ore 19.30 alle 21.00, nella BBX_Virtual_Dive#3, il socio Mattia Musella ha tenuto la prima presentazione dedicata alla "Subacquea culturale! Come è nata la nostra bellissima passione? Storia della subacquea: fatti, miti, leggende e curiosità del passato”. <br> Durante la presentazione Mattia Musella ha affrontato la storia della subacquea, con una brillante narrazione ricca di dettagli, con riferimenti alla geografia, all’astronomia e alla mitologia.<br> Così ha descritto minuziosamente l’equipaggiamento dei primi subacquei della storia, i “tuffatori” o pescatori, dei quali le prime rappresentazioni sono dipinti o incisioni su pareti, tombe, vasi assiri, babilonesi, egiziani e etruschi, riportando le storie dei primi racconti di questi esploratori subacquei che risalgono ad Aristotele (IV sec AC) e ad Oppiano (II sec DC). <br>',
+	img: "img/news/storia1.png",
+	link: "http://www.bigbluexplorers.com/"
+    } 
+    ,    
+    {
+	id : 9 ,
+	type : 'education',
+	date : '2020/04/15',
+	title : "BBX Virtual Dive #4",
+	body : 'Mercoledì 15 aprile 2020, dalle ore 19.30 alle 21.00, i soci della BigBlueXplores si sono incontrati virtualmente  per la BBX_Virtual_Dive#4.<br> Il socio Nicola Rossi ha presentato "DECO &amp; COMPUTER, Una chiacchierata sulla teoria degli algoritmi decompressivi nei computer subacquei”. <br> L’obiettivo era condividere un po’ di conoscenza su come i più diffusi algoritmi decompressivi sono implementati nei computer subacquei, cercando di comprendere i parametri che caratterizzano un profilo decompressivo.<br> Modelli decompressivi Buhlmann e VPM, M – Values, gradient factors e conservativismi, etc.<br> Grazie Nicola per il tuo intervento! Davvero molto utile per avvicinarsi in modo più semplice ad argomenti comunque complessi.<br>',
+	img: "img/news/deco.jpg",
+	link: "http://www.bigbluexplorers.com/"
+    }
+    ,
+    
+    {
+	id : 10 ,
+	type : 'education',
+	date : '2020/04/22',
+	title : "BBX Virtual Dive #5",
+	// <a href=\"\"></a>
+	body : "Mercoledì 22 aprile 2020, dalle ore 19.30 alle 21.00, i soci della BigBlueXplores si sono incontrati virtualmente  nella BBX_Virtual_Dive#5, per la presentazione da parte del socio Mattia Musella sulla Storia della subacquea, subacquea culturale &amp; curiosa, parte 2:<br> -Cosa ci siamo persi da Leonardo in poi<br> -Passeggiate subacquee romane ...medievali <br> -Subacquea ed (una sola) equazione -Subacquei di giorno, astronomi di notte (un caso curioso) <br> -Orgoglio italico sub <br> -Ripassiamo un po' il Nautilus <br> -Maiali subacquei (senza offesa eh) <br> -Le prime tabelle deco (proprio quelle...mai viste?) -Subacquea si....ma a bassa pressione<br> Il socio Mattia Musella, che già aveva creato molto interesse nella parte 1, ha superato questi risultati con la presentazione relativa alla seconda parte, guidando i partecipanti lungo tutto il percorso storico che dalle prime origini della subacquea risalenti ai cd. “Tuffatori”, cui fecero seguito i “Pescatori” e successivamente gli  “Urinatores Romani”, si è poi sviluppato da Leonardo da Vinci in avanti dapprima con i “primi Palombari”, seguiti dall’utilizzo delle prime “Campane Subacquee”, fino ad arrivare ai più moderni Palombari e infine ai moderni subacquei.<br> Le prime pinne compaiono nel 1679 con un modello realizzato da Giovanni Borelli. Il primo scafandro da palombaro molto simile a quello odierno si deve ad un ingegnere inglese Christian Augustus Siebe, con uno scafandro comprendente un costume stagno e un elmo amovibile.<br> Nel 1878 Henry Fleuss a Londra realizzò il primo ARO a circuito chiuso e lo montò su uno scafandro, ma l’immersione era molto rischiosa e la mancanza di conoscenza della fisiologia creò diversi incidenti.<br> Nel 1912 Haldane pubblica le sue tabelle per la decompressione e i suoi studi ancora oggi rappresentano la base della decompressione, da qui un cenno storico ai cd. “Maiali Subacquei” siluri a lenta corsa, che altro non erano che dei sommergibili tascabili di forma simile a un siluro, adatti a trasportare, a bassa velocità, due operatori muniti di respiratori subacquei autonomi e una carica esplosiva da applicare alla carena della nave avversaria che si trovava all'ormeggio. <br> La presentazione termina con l’avvento della subacquea moderna, che comunque vede le svolta decisiva nel 1943 con  la creazione della prima attrezzatura per lo scuba diving ad opera di Jacques-Yves Costeau ed Emile Gagnan.<br> Grazie Mattia!!!!!",
+	img: "img/news/storia2.png",
+	link: "http://www.bigbluexplorers.com/"
+    },
+
+    {
+	id : 11 ,
+	type : 'education',
+	date : '2020/04/29',
+	title : "BBX Virtual Dive #6",
+	body : 'Mercoledì 29 aprile 2020, dalle ore 19.30 alle 21.00, i soci della BigBlueXplores si sono incontrati virtualmente nella BBX_Virtual_Dive#6, per la presentazione da parte di Luca Palezza sul tema “Introduzione alla fotogrammetria e sue applicazioni”.<br> Luca fa parte di Ocean Explorers, un team di subacquei dedicato alla scoperta e alla conservazione del nostro patrimonio culturale subacqueo. <br> Un tema dedicato ad esplorare i fondali dei nostri mari alla ricerca del nostro patrimonio culturale subacqueo, documentando e studiando le scoperte, cercando di aiutare archeologi e scienziati a preservare il nostro passato e dargli un nuovo futuro luminoso. <br> La fotogrammetria è la tecnica che utilizza le immagini fotografiche di un oggetto per ricavarne le dimensioni. Consente di correggere le distorsioni dei fotogrammi e trasformare la visione prospettica dell’oggetto ripreso in una proiezione ortogonale bidimensionale, oppure il medesimo oggetto è fotografato da due punti differenti in modo da ricavarne una visione tridimensionale.<br> Questo consente di presentare l’oggetto ripreso apprezzando i valori di quota delle sue varie parti, cioè le distanze dall’obiettivo della fotocamera.  <br> Questo tipo di fotografia ha trovato di recente una larga applicazione nella fotografia subacquea dando la possibilità a ricercatori e studiosi di poter osservare reperti archeologici, altri oggetti o parti di fondale o addirittura relitti di aerei o navi, come se i ricercatori si trovassero effettivamente in immersione.<br> Queste immagini 3D vengono poi talvolta animate e inserite all’interno di video a scopo divulgativo per chiarire meglio ad esempio l’utilizzo nell’antichità di un determinato reperto.<br> Luca ha dapprima introdotto l’argomento in generale, passando poi attraverso le difficoltà della realizzazione in acqua delle fotografie, l’utilizzo dei diversi software, per finire con una carrellata delle possibili applicazioni presentandole attraverso una serie di casi concreti (video, mostre, visori 3D, etc.).<br>',
+	img: 'img/news/fotogrammetria.jpg',
+	link: 'https://www.facebook.com/luca.palezza'
     }
 
     /*
