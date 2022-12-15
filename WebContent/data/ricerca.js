6963aa3ed0a1002c60994b43afd9ec3924d42c2a
Nicola <nicola.rossi+2@gmail.com>
2022-12-15 22:30:41 +0100

    Change

---
+++ WebContent/data/ricerca.js
@@ -0,0 +1,155 @@
+
+
+paper_list=[
+    {
+	id : 6 ,
+	authors : '<u>Romano E.</u>, Bergamin L., Di Bella L., Baini M., Berto D., <u>D’Ambrosi A.</u>, Di Fazio M., Galli M., Medeghini L., Panti C., <u>Provenzani C.</u>, Rampazzo F., Fossi M.C.',	
+	date : '2023',
+	research_title : "First record of microplastic in the environmental matrices of a Mediterranean marine cave (Bue Marino, Sardinia, Italy)",
+	where : 'Marine Pollution Bulletin, 186, 114452',
+	link : 'https://doi.org/10.1016/j.marpolbul.2022.114452'	
+    },
+
+
+    {
+	id :  5,
+	authors : '<u>Romano E.</u>, Bergamin L., Di Bella L., Frezza V., Pierfranceschi G., Marassich A., <u>Provenzani C.</u>',
+	date : '2021',
+	research_title : "Benthic foraminifera as environmental indicators in extreme environments: the marine cave of Bue Marino (Sardinia, Italy)",
+	where : 'Ecological Indicators, 120, 106977 ',
+	link : 'https://doi.org/10.1016/j.ecolind.2020.106977'	
+    },
+
+        {
+	id :  4,	
+	authors : '<u>Romano E.</u>, Bergamin L., Di Bella L., Frezza V., Marassich A., Pierfranceschi G., <u>Provenzani C.</u>',
+	date : '2020',
+	research_title : "Benthic foraminifera as proxies of marine influence in the Orosei marine caves (Sardinia, Italy)",
+	where : 'Aquatic Conservation: Marine and Freshwater Ecosystems, 30, 701-716 ',
+	link : 'https://doi.org/10.1002/aqc.3288 '	
+    },
+    {
+	id :  3,	
+	authors : 'Bergamin L., Taddei Ruggiero E., Pierfranceschi G., Andres B., Armiento G., Costantino R., Crovato C., <u>D’Ambrosi A.</u>, Marassich A., <u>Romano E.</u>',
+	date : '2020',
+	research_title : "Benthic foraminifera and brachiopods from a marine cave in Spain: environmental significance.",
+	where : 'Mediterranean Marine Science, 21 (3), 506-518. ',
+	link : 'https://doi.org/10.12681/mms.23482 '	
+    },
+    {
+	id :  2,
+	
+	authors : 'Bergamin L., Marassich A., <u>Provenzani C.</u>, <u>Romano E.</u>',
+	date : '2018',
+	research_title : "Foraminiferal ecozones in two submarine caves in the Orosei Gulf (Sardinia, Italy)",
+	where : 'Rendiconti Lincei, 29 (3), 547-557',
+
+    },
+    {
+	id : 1 ,
+	
+	authors : '<u>Romano E.</u>, Bergamin L., Pierfranceschi G., <u>Provenzani C.</u>, Marassich A.',
+	date : '2018',
+	research_title : "The distribution of benthic foraminifera in Bel Torrente submarine cave (Sardinia, Italy) and their environmental significance",
+	where : 'Marine Environmental Research, 133, 114-127 ',
+	link : 'https://doi.org/10.1016/j.marenvres.2017.12.014'	
+    },
+    
+];
+
+
+
+poster_list=[
+
+    {
+	id :  12 ,
+	
+	authors : '<u>Romano E.</u>, Bergamin L., Di Bella L., <u>D’Ambrosi A.</u>, Gaglianone G., Pierdomenico M., <u>Provenzani C.</u>',
+	date : '2022',
+	research_title : "Preliminary results on benthic foraminifera from sediments of the Argentarola marine cave (Tyrrhenian Sea).",
+	where : 'Annual meeting MEDGU “Mediterranean Geosciences Union”. Marrakech (Morocco), 27-30 November 2022',
+
+    },
+
+        {
+	id : 11 ,
+	
+	authors : '<u>Romano E.</u>, Baini M., Bergamin L., <u>D’Ambrosi A.</u>, Di Bella L., Panti C., <u>Provenzani C.</u>, Fossi M.C.',
+	date : '2022',
+	research_title : "Preliminary study on microplastics and their potential effects on benthic foraminifera in the Bue Marino cave (East Sardinia, Italy)",
+	where : '5th International Symposium on Anchialine Ecosystems (5ISAE). Kailua, Kona, Hawaii, 3-5 November 2022',
+    },
+    {
+	id :  10,	
+	authors : '<u>Romano E.</u>, Bergamin L., De Santis C., Di Bella L., Frezza V., Marassich A., <u>Provenzani C.</u>',
+	date : '2021',
+	research_title : "Paleoecological reconstruction and modern analogues from foraminiferal proxies in the Middle Branch of Bue Marino Cave (Sardinia, Italy) - Preliminary results",
+	where : '90° Convegno della Società Geologica Italiana, 14-16 settembre 2021',
+    },
+    {
+	id : 9 ,
+	
+	authors : '<u>Romano E.</u>, Bergamin L., Pierfranceschi G., Marassich A., <u>Provenzani C.</u>',
+	date : '2021',
+	research_title : "Environmental variability in the marine environment: insights from benthic foraminifera of marine caves.",
+	where : '4° Convegno dei Geologi Marini Italiani, 25-26 febbraio 2021',
+    },
+    {
+	id :  8,	
+	authors : '<u>Provenzani C.</u>, Salvati E., Santero W., <u>D’Ambrosi A.</u>, Leonini F., <u>Romano E.</u>',
+	date : '2019',
+	research_title : "In situ long term observation of Dendrophyllia ramea.",
+	where : '5th European Conference on Scientific Diving. Sopot (Poland), 24-27 April 2019.',
+    },
+    {
+	id :  7,	
+	authors : '<u>Romano E.</u>, Bergamin L., Di Bella L., Frezza V., Marassich A., <u>Provenzani C.</u>',
+	date : '2018',
+	research_title : "Benthic foraminifera as proxies of environmental variability in the Orosei submarine cave system (Sardinia, Italy).",
+	where : 'International Congress FORAMS 2018. Edinburgh (UK), 17-22 June 2018',
+    },
+    {
+	id :  6,	
+	authors : '<u>Romano E.</u>, Bergamin L., Marassich A., Mo G., <u>Provenzani C.</u>',
+	date : '2018',
+	research_title : "Scientific diving for the study of Mediterranean cave environments: from protists to mammals.",
+	where : '4th European Conference on “Scientific Diving”. Orkney islands (UK), 9-12 April 2018.',
+    },
+    {
+	id :  5,	
+	authors : '<u>Provenzani C.</u>, <u>Appodia F.</u>, <u>Corirossi M.</u>, Gassi M.A., <u>Papini M.</u>, <u>Pasanisi E.</u>, Ricci E., <u>Romano E<.u>, <u>Rossi N.</u>',
+	date : '2018',
+	research_title : "Project Baseline Tyrrhenian Sea as citizen scienze project for supporting the knowledge about marine areas status.",
+	where : '4th European Conference on “Scientific Diving”. Orkney islands (UK), 9-12 April 2018.',
+    },
+    {
+	id :  4,	
+	authors : 'Bergamin L., Marassich A., <u>Provenzani C.</u>, <u>Romano E.</u>',
+	date : '2017',
+	research_title : "Foraminiferal ecozones in two submarine caves in the Orosei Gulf (Sardinia, Italy)",
+	where : 'Accademia dei Lincei – XXX Giornata dell’Ambiente. Convegno “Cambiamenti e crisi nel Mediterraneo”. Roma, 17 Ottobre 2017',
+    },
+    {
+	id :  3,	
+	authors : '<u>Romano E.</u>, Bergamin L., Pierfranceschi G., Berto D., <u>Provenzani C.</u>, Marassich A.',
+	date : '2015',
+	research_title : "The environmental characterization in Bue Marino cave (Sardinia, Italy) using benthic foraminifera.",
+	where : 'International Congress “GeoSUB – Underwater geology”. Trieste, 13-14 October 2015',
+    },
+    {
+	id :  2,	
+	authors : 'Mo G., <u>Provenzani C.</u>, Marassich A., <u>Romano E.</u>',
+	date : '2015',
+	research_title : "Characterization of Mediterranean monk seal (Monachus monachus) bones in Bel Torrente cave (Sardinia, Italy)",
+	where : 'International Congress “GeoSUB – Underwater geology”. Trieste, 13-14 October 2015',
+    },
+    {
+	id :  1,	
+	authors : 'Bergamin L., <u>Romano E.</u>, Celia Magno M., Berto D., <u>Provenzani C.</u>, Marassich A.',
+	date : '2015',
+	research_title : "The environmental characterization in Bue Marino cave (Sardinia, Italy) using benthic foraminifera.",
+	where : 'International Congress “GeoSUB – Underwater geology”. Trieste, 13-14 October 2015.',
+    },
+    
+];
+
--- WebContent/src/news.js
+++ WebContent/src/news.js
@@ -1,22 +1,30 @@
 
-function load_news( just_first ) {
+function load_news( just_first, target_container, target_list ) {
     var type_inserted={};
     
 /*    news_list.sort(function (a,b) {
      	return (a.id > b.id);
     });*/
 
-   // console.log(" dentro load_news "+news_list.length+ ' ' +just_first);
-   
-    for (var i=0; i<news_list.length; i++) {
+    // console.log(" dentro load_news "+news_list.length+ ' ' +just_first);
+    target_list.sort(function(a, b) {
+	keyA= a.id ;
+	keyB= b.id ;
+	if (keyA > keyB) return -1;
+	if (keyA < keyB) return 1;
+	return 0;
+    });
+
+    pencil='<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>'
+    
+    for (var i=0; i<target_list.length; i++) {
 	
-//	console.log(" l = "+(news_list.length-1 -i  i)+" "+i);
-	
-	var news=news_list[ news_list.length-1 -  i ];
-	if ( ! ("type" in news) ) {
-	    console.log('ATTENZIONE l elemento '+news.id+' non ha type');
-	    return;
-	}
+	//	console.log(" l = "+(target_list.length-1 -i  i)+" "+i);
+	var news=target_list[ i ];
+	// if ( ! ("type" in news) ) {
+	//     console.log('ATTENZIONE l elemento '+news.id+' non ha type');
+	//     return;
+	// }
 //	console.log(" dentro load_news "+i+' : '+JSON.stringify(news));
 
 	/**/
@@ -29,17 +37,30 @@ function load_news( just_first ) {
 	var icon=''
 	if ( 'icon' in news) {
 	    icon+='<span class="'+news.icon+'" aria-hidden="true"></span>'
-	} else {
+	} else if ('type' in news) {
 	    icon+='<span class="glyphicon glyphicon-'+news.type+'" aria-hidden="true"></span>'
+	    
+
 	}
-	header.html("<strong><h2 id=\""+news.id +"\">"+icon+' '+news.title+"</h2></strong>");
+	if ('title' in news) {
+	    header.html("<strong><h2 id=\""+news.id +"\">"+icon+' '+news.title+"</h2></strong>");
+	}
+	
+	if ('authors' in news) {
+	    //	    header.html("<p class='font-italic'><h2 id=\""+news.id +"\">"+news.authors+"</h2></italic>");
+	    header.html(pencil+" <p class='font-italic'>"+news.authors+" <b>"+ news.date+"</b> </p>");
+	}
 
 	var body=$("<div class='panel-body'></div>");
 
 	var html=" "
 	var col_text=12
+
 	var col_img=4
 	html+='<div class="row">'
+	if ('research_title' in news){
+	    html+='<div class="col-xs-'+col_text+'">'+news.research_title+'</div>'	    
+	}
 	if ('img' in news) {
 	    col_text=8
 	}
@@ -61,33 +82,34 @@ function load_news( just_first ) {
 	if ('vimeo' in news) {
 	    html+="<div class='embed-container'><iframe src='"+news.vimeo+"' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>"
 	}
+	if ('where' in news) {
+	    html+=(" <p class='font-italic'>"+news.where+" "+ news.date +"</p>");
+	}  
 	html+='</div>'
 	
 	body.html(html);
 
-	var footer=$("<div class='panel-footer'></div>");
-	html_footer=""
-	if ('date' in news) {
-	    html_footer+=("<strong>"+news.date+"</strong><br>");
-	} 
-//	html_footer+="<a href=\"whatsapp://send?text="+news.title+"\" data-action=\"share/whatsapp/share\"><span class=\"glyphicon glyphicon-share\"></span> Condividi via Whatsapp</a>"	 ;
-
-	
-	
-//	html_footer+="<a href=\"whatsapp://send\" data-text=\""+news.title.replace(/<[^>]+>/g,"")+"\" data-href=\"#"+news.id+"\" <span class=\"glyphicon glyphicon-share\"></span> Condividi via Whatsapp</a>"	 ;
-
-//	html_footer+="<a href=\"whatsapp://send?text=text to share\" data-action=\"share/whatsapp/share\"><span class=\"glyphicon glyphicon-share\"></span> Condividi via Whatsapp</a>"	 ;
-	
-//	<a href="whatsapp://send" data-text="Take a look at this awesome website:" data-href="http://www.pippo.com" class="wa_btn wa_btn_s" style="display:none">Share</a>
-	footer.html(html_footer);
-	panel.append(header,body,footer);
+	if ('type' in news) {
+	    var footer=$("<div class='panel-footer'></div>");
+	    html_footer=""
+	    if ('date' in news) {
+		html_footer+=("<strong>"+news.date+"</strong>");
+	    }
+	    html_footer+="</br>"
+	    
+	    
+	    footer.html(html_footer);
+	    panel.append(header,body,footer);
+	} else {
+	    panel.append(header,body);
+	}
 
 	el.append(panel);
 	
 	/**/
 
 	if ( (! just_first) || ! ( type_inserted.hasOwnProperty(news.type) )) {
-	    $("#news_container").append(el);
+	    $("#"+target_container).append(el);
 	    type_inserted[news.type]= true;
 	}
 	
@@ -101,3 +123,14 @@ function load_news( just_first ) {
 }
 
 
+
+
+//	html_footer+="<a href=\"whatsapp://send?text="+news.title+"\" data-action=\"share/whatsapp/share\"><span class=\"glyphicon glyphicon-share\"></span> Condividi via Whatsapp</a>"	 ;
+
+	
+	
+//	html_footer+="<a href=\"whatsapp://send\" data-text=\""+news.title.replace(/<[^>]+>/g,"")+"\" data-href=\"#"+news.id+"\" <span class=\"glyphicon glyphicon-share\"></span> Condividi via Whatsapp</a>"	 ;
+
+//	html_footer+="<a href=\"whatsapp://send?text=text to share\" data-action=\"share/whatsapp/share\"><span class=\"glyphicon glyphicon-share\"></span> Condividi via Whatsapp</a>"	 ;
+	
+	//	<a href="whatsapp://send" data-text="Take a look at this awesome website:" data-href="http://www.pippo.com" class="wa_btn wa_btn_s" style="display:none">Share</a>
